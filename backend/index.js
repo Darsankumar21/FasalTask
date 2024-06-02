c9d2db2140fde0b60b7f678ef119b3bb46b19b02
Darsankumar21 <93649735+Darsankumar21@users.noreply.github.com>
2024-06-03 00:13:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin: ['https://fasal-task-l2tr-4nixrm8vy-darsankumars-projects.vercel.app'],
+    origin: ['*'],
     credentials: true
 }
 app.use(cors(corsOptions));
